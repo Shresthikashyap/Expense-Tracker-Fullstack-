6fdd5c10a18bd1e74ea8d64181b667ff38a50669
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-07-17 22:08:10 +0530

    Update Login.js

--- frontend/src/components/AuthForm/Login.js
+++ frontend/src/components/AuthForm/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
         setError(null);
 
         if(enteredEmail !== null && enteredPassword !== null){
-            const response = await axios.post("https://mail-box-client-c2vn.onrender.com/users/login", {
+            const response = await axios.post("http://localhost:3001/users/login", {
                 email: enteredEmail,
                 password: enteredPassword
             }); 
