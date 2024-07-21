efaeec2e247a3725a41a70c46cdb0ceb9d168422
Shresthi Kashyap <sk2929542@gmail.com>
2024-07-21 16:38:50 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,11 @@ const multer = require('multer')
 //const morgan = require('morgan');
 //const helmet = require('helmet')
 
-const errorController = require('./controllers/error');
+<<<<<<< HEAD
+//const errorController = require('./controllers/error');
 
+=======
+>>>>>>> 38b4b7db37d83adc082d85ad4dd91755a2abf21f
 const userRoutes = require('./routes/user');
 const premiumUser = require('./routes/premium');
 const expenseRoutes = require('./routes/expense');
@@ -49,7 +52,10 @@ app.use('/premium',premiumUser);
 app.use('/password',resetPasswordRoutes);
 app.use('/download',downloadRoutes);
 app.use('/downloadedFiles',allDownloadedFiles);
-app.use('/*', errorController.get404);
+<<<<<<< HEAD
+//app.use('/*', errorController.get404);
+=======
+>>>>>>> 38b4b7db37d83adc082d85ad4dd91755a2abf21f
 app.use((req,res) => {
     console.log('Request url ',req.url);
     console.log('request is successful')
