fb21bac2f15536b753672de6287956ea6a9212db
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-07-21 16:48:05 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,6 @@ const multer = require('multer')
 
 //const errorController = require('./controllers/error');
 
-=======
->>>>>>> 38b4b7db37d83adc082d85ad4dd91755a2abf21f
 const userRoutes = require('./routes/user');
 const premiumUser = require('./routes/premium');
 const expenseRoutes = require('./routes/expense');
@@ -51,10 +49,8 @@ app.use('/premium',premiumUser);
 app.use('/password',resetPasswordRoutes);
 app.use('/download',downloadRoutes);
 app.use('/downloadedFiles',allDownloadedFiles);
-<<<<<<< HEAD
 //app.use('/*', errorController.get404);
-=======
->>>>>>> 38b4b7db37d83adc082d85ad4dd91755a2abf21f
+
 app.use((req,res) => {
     console.log('Request url ',req.url);
     console.log('request is successful')
